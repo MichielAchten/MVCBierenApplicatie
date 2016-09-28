83aea05913964cf5e2e6723b7d736791eddb0492
MichielAchten <michiel.achten@gmail.com>
2016-09-28 15:50:02 +0200

    OefeningTempDataEnRedirects1

--- MVCBierenApplicatie/Controllers/BierController.cs
+++ MVCBierenApplicatie/Controllers/BierController.cs
@@ -12,17 +12,38 @@ namespace MVCBierenApplicatie.Controllers
     {
         // GET: Bier
 
-        private BierService bierservice = new BierService();
+        private BierService bierService = new BierService();
         
         public ActionResult Index()
         {
-            var bieren = bierservice.FindAll();
+            var bieren = bierService.FindAll();
             return View(bieren);
 
             //List<Bier> bieren = new List<Bier>();
             //bieren.Add(new Bier { ID = 15, Naam = "Felix", Alcohol = 0.07f });
             //bieren.Add(new Bier { ID = 17, Naam = "Roman", Alcohol = 0.075f });
             //return View(bieren);
+        }
+
+        public ActionResult Verwijderen(int id)
+        {
+            var bier = bierService.Read(id);
+            return View(bier);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var bier = bierService.Read(id);
+            this.TempData["bier"] = bier;
+            bierService.Delete(id);
+            return Redirect("~/Bier/Verwijderd");
+        }
+
+        public ActionResult Verwijderd()
+        {
+            var bier = (Bier)this.TempData["bier"];
+            return View(bier);
         }
     }
 }
--- MVCBierenApplicatie/Services/BierService.cs
+++ MVCBierenApplicatie/Services/BierService.cs
@@ -12,17 +12,27 @@ namespace MVCBierenApplicatie.Services
 
         static BierService()
         {
-            //bieren[1] = new Bier { ID = 15, Naam = "Felix", Alcohol = 0.07f };
-            //bieren[2] = new Bier { ID = 17, Naam = "Roman", Alcohol = 0.075f };
-            bieren[3] = new Bier { ID = 1, Naam = "Cnudde kriek", Alcohol = 4.7f };
-            bieren[4] = new Bier { ID = 2, Naam = "Liefmans goudenband", Alcohol = 8f };
-            bieren[5] = new Bier { ID = 3, Naam = "Sloeber", Alcohol = 7.5f };
-            bieren[6] = new Bier { ID = 4, Naam = "Felix kriekbier", Alcohol = 5f };
+            //bieren[15] = new Bier { ID = 15, Naam = "Felix", Alcohol = 0.07f };
+            //bieren[17] = new Bier { ID = 17, Naam = "Roman", Alcohol = 0.075f };
+            bieren[1] = new Bier { ID = 1, Naam = "Cnudde kriek", Alcohol = 4.7f };
+            bieren[2] = new Bier { ID = 2, Naam = "Liefmans goudenband", Alcohol = 8f };
+            bieren[3] = new Bier { ID = 3, Naam = "Sloeber", Alcohol = 7.5f };
+            bieren[4] = new Bier { ID = 4, Naam = "Felix kriekbier", Alcohol = 5f };
         }
 
         public List<Bier> FindAll()
         {
             return bieren.Values.ToList();
+        }
+
+        public Bier Read(int id)
+        {
+            return bieren[id];
+        }
+
+        public void Delete(int id)
+        {
+            bieren.Remove(id);
         }
     }
 }
